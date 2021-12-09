177110bf3c8b080b1b5e3d23b0e7d43c06ff18a7
anwerr2001 <anwerr2001@hotmail.com>
2021-12-09 16:10:48 -0500

    change AMI ID

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   profile = "default"
 
 resource "aws_instance" "example" {
-  ami           = "ami-0b17e49efb8d755c3"
+  ami           = "ami-0b17e49efb8d755c4"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-05c6053ba0391e4ed"]
   subnet_id   = "subnet-085fb56d2dc3965f1"
