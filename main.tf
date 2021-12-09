4edddc78550969777b1a72735767c0cb76036a30
anwerr2001 <anwerr2001@hotmail.com>
2021-12-09 16:44:12 -0500

    change instance config

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ provider "aws" {
   profile = "default"
 
 resource "aws_instance" "example" {
-  ami           = "ami-0b17e49efb8d755c4"
+  ami           = "ami-0b17e49efb8d755c3"
   instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-05c6053ba0391e4ed"]
-  subnet_id   = "subnet-085fb56d2dc3965f1"
+  vpc_security_group_ids = ["sg-0cf69f17137c3ebde"]
+  subnet_id   = "subnet-0175c8240d3f9a986"
    tags = {
       "Name" = "TEST-EC2-5"  }
   }
